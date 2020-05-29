47c18000266fb59d2472b86ffcf8747762bad2b3
Andy <awzbball@gmail.com>
2020-05-29 00:42:31 -0700

    imported appropriate libraries

---
+++ train.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import keras
+# import tensorflow as tf
+# import torch
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+plt.plot(range(10), stats.poisson.pmf(range(10), 2))
+plt.show();
